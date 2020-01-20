cef1f95512d47dfc9f8c8a4af491877408b9fa57
Daniel_Perwejnis <daniel.perwejnis@gmail.com>
2020-01-20 15:59:50 +0100

    Implemented simple Bank Web API

---
+++ Bank.API/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Bank.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bank.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomersController : Controller
+    {
+        private readonly BankContext ctx;
+
+        public CustomersController(BankContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return ctx.Customers;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(ctx.Customers.Find(id));
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]Customer customer)
+        {
+            ctx.Customers.Add(customer);
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(long id)
+        {
+            ctx.Customers.Remove(ctx.Customers.Find(id));
+        }
+    }
+}
