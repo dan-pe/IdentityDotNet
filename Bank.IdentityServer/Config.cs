f536732ce9bf49553b04363ca507033e41aa7b1e
Daniel_Perwejnis <daniel.perwejnis@gmail.com>
2020-01-21 14:47:20 +0100

    Add IdentityServer project

---
+++ Bank.IdentityServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Bank.IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetAllApiResourses()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("bankApi", "Customer Api for Bank")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "bankApi" }
+                }
+            };
+        }
+    }
+}
